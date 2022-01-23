58790dd3e96078dd79a4c360a47fb493d90f6ff6
Manish Sabbani <Manishsabbani1998@gmail.com>
2022-01-23 20:15:58 +0530

    else if condition x>=201 changes the num

--- currentBillCode.js
+++ currentBillCode.js
@@ -1,5 +1,5 @@
 
-let x = 350;
+let x = 500;
   if(x<=50) {
       console.log(x*3);
   } else if(x<=100) {
@@ -8,7 +8,7 @@ let x = 350;
       console.log((50*3)+(50*3.50)+(x-100)*4);
   }else if(x<=200) {
       console.log((50*3)+(50*3.50)+(50*4)+(x-150)*4.50);
-  }else if(x>=250) {
+  }else if(x>=201) {
       console.log((50*3)+(50*3.50)+(50*4)+(50*4.50)+(x-200)*4.75);
   }else  {
       console.log("please enter a valid number")
