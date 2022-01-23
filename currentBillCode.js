d1419d2c0a8789f1f58faed8f49d95f9df03b88d
Manish Sabbani <Manishsabbani1998@gmail.com>
2022-01-23 20:35:02 +0530

    x renaming as units and change the value 0, Removing the console.log() and add the amount assign the values in the if and else if statement

--- currentBillCode.js
+++ currentBillCode.js
@@ -1,17 +1,17 @@
 
-let x = 500;
+let x = 0;
 var amount;
   if(units>0) {
   if(units<=50) {
-      console.log(amount = units*3);
+      amount = units*3;
   } else if(units<=100) {
-      console.log(amount = 50*3+(units-50)*3.50);
+      amount = 50*3+(units-50)*3.50;
   } else if(units<=150) {
-      console.log((50*3)+(50*3.50)+(units-100)*4);
+      amount = (50*3)+(50*3.50)+(units-100)*4;
   }else if(units<=200) {
-      console.log((50*3)+(50*3.50)+(50*4)+(units-150)*4.50);
+      amount = (50*3)+(50*3.50)+(50*4)+(units-150)*4.50;
   }else if(units>=201) {
-      console.log((50*3)+(50*3.50)+(50*4)+(50*4.50)+(units-200)*4.75);
+      amount = (50*3)+(50*3.50)+(50*4)+(50*4.50)+(units-200)*4.75;
   }else  {
       console.log("please enter a valid number");
   }
